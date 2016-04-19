dc1311905194e15277a876535e85abce8e569777
Jackrim1 <jriminton@gmail.com>
2016-04-19 11:32:42 +0100

    Jack - Challenge 5 top up complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,10 @@ describe Oystercard do
 	it "shows the balance" do
   expect(subject.balance).to eq 0
 	end
+
+  it "tops up the balance" do
+    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+  end
+
+
 end

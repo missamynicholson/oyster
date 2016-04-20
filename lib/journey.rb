7ca7487c127cbf2d7ed55b995e22d7ce7cfb4a89
Amy Nicholson <missamynicholson@gmail.com>
2016-04-20 20:00:10 +0100

    Challenge 14 - improved

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,10 +1,11 @@
 class Journey
+  MINIMUM_FARE = 1
+  attr_reader :entry_station, :list_of_journeys, :journey_complete
 
-  attr_reader :entry_station, :list_of_journeys
-
-  def initialize
-    @entry_station = nil
+  def initialize(entry_station = nil)
+    @entry_station = entry_station
     @list_of_journeys = []
+    @journey_complete = nil
   end
 
   def start(entry_station)
@@ -12,28 +13,23 @@ class Journey
   end
 
   def finish(exit_station)
-    calculate(exit_station)
+    fare(exit_station)
   end
 
   def in_journey?
     !!entry_station
   end
 
-  def calculate(exit_station)
-    fail "You haven't touched in" if entry_station.nil?
-    hash = {}
-    hash[entry_station] = exit_station
+  def fare(exit_station)
+    6 if entry_station.nil? || exit_station.nil?
     @entry_station = nil
-    @list_of_journeys << hash
-    zone_calculate(list_of_journeys)
-  end
-
-  def zone_calculate(list_of_journeys)
-    20
+    @list_of_journeys << {entry_station => exit_station}
+    @journey_complete = true
+    MINIMUM_FARE
   end
 
   def journey_complete?
-    @list_of_journeys.last
+    @journey_complete
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -11,56 +11,50 @@ describe Journey do
 
   it "ends a journey" do
     journey.start(station)
-    expect(journey.finish(station)).to eq 20
+    expect(journey.finish(station)).to eq Journey::MINIMUM_FARE
   end
 
   context "Card has enough money" do
 
-      it "confirms user is not in journey" do
+      it "confirms user is not in journey at start" do
         expect(journey).not_to be_in_journey
       end
 
-      it "touches card in" do
+      it "starts journey" do
         journey.start(station)
         expect(journey).to be_in_journey
       end
 
-      it "touches card out" do
+      it "ends journey" do
         journey.start(station)
         journey.finish(station)
         expect(journey).not_to be_in_journey
       end
 
-      it "forgets station on touch out" do
+      it "forgets entry station when journey ends" do
         journey.start(station)
         journey.finish(station)
         expect(journey.entry_station).to be_nil
       end
 
-      it "raises an error if not touched in" do
-        expect {journey.finish(station)}.to raise_error "You haven't touched in"
+      it "raises an error if didn't touch in" do
+        expect(journey.finish(station)).to eq 1
+      end
+
+      it "raises an error if didn't touch out" do
+        expect(journey.finish(nil)).to eq 1
+      end
+
+      it "returns journey not complete" do
+        expect(journey).not_to be_journey_complete
+      end
+
+      it "returns journey is complete" do
+        journey.start(station)
+        journey.finish(station)
+        expect(journey).to be_journey_complete
       end
 
     end
-
-
-# context "completed journeys" do
-
-#   let(:journey){ {entry_station => exit_station} }
-
-#     before do
-#       subject.top_up(Oystercard::MINIMUM_BALANCE)
-#     end
-
-#     it "Checks that the journey list is empty before travelling" do
-#       expect(subject.list_of_journeys).to be_empty
-#     end
-
-#     it "is a hash consisting of entry and exit stations" do
-#       oystercard_topped.touch_out(exit_station)
-#       expect(oystercard_topped.list_of_journeys).to include journey
-#     end
-
-# end
 
 end
